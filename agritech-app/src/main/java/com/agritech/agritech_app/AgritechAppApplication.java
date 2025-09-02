56c399558c8007206502d7b957de2cdc8200a0c0
Tanu <mukund.pokale@outlook.com>
2025-09-02 23:28:55 +0530

    c

--- agritech-app/src/main/java/com/agritech/agritech_app/AgritechAppApplication.java
+++ agritech-app/src/main/java/com/agritech/agritech_app/AgritechAppApplication.java
@@ -2,15 +2,13 @@ package com.agritech.agritech_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
-import com.agritech.agritech_app.entity.User;
-import com.agritech.agritech_app.repositery.UserRepository;
 
 @SpringBootApplication
 public class AgritechAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgritechAppApplication.class, args);
+		
 	}
 }
